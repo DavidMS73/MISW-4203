d4eecb3f157396b38cd55c7bc78a2ef743461c20
DavidMS73 <gd.martinez@uniandes.edu.co>
2025-05-09 07:38:23 -0500

    Memory optimizations

--- app/src/main/java/com/example/vinilos/ui/components/ImageCard.kt
+++ app/src/main/java/com/example/vinilos/ui/components/ImageCard.kt
@@ -1,5 +1,6 @@
 package com.example.vinilos.ui.components
 
+import android.content.Context
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,8 +16,13 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.ImageLoader
 import coil.compose.AsyncImage
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
 import coil.request.ImageRequest
+import coil.util.DebugLogger
 import com.example.vinilos.R
 
 @Composable
@@ -32,7 +38,12 @@ fun ImageCard(
         AsyncImage(
             model = ImageRequest.Builder(context = LocalContext.current)
                 .data(imageUrl)
-                .crossfade(true).build(),
+                .crossfade(true)
+                .memoryCacheKey(imageUrl)
+                .diskCacheKey(imageUrl)
+                .diskCachePolicy(CachePolicy.ENABLED)
+                .memoryCachePolicy(CachePolicy.ENABLED)
+                .build(),
             contentDescription = null,
             placeholder = painterResource(R.drawable.loading_img),
             error = painterResource(R.drawable.ic_broken_image),
@@ -40,7 +51,8 @@ fun ImageCard(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(162.dp)
-                .clip(RoundedCornerShape(8.dp))
+                .clip(RoundedCornerShape(8.dp)),
+            imageLoader = customImageLoader(context = LocalContext.current)
         )
         Text(
             text = title,
@@ -48,4 +60,22 @@ fun ImageCard(
             modifier = Modifier.padding(top = 8.dp)
         )
     }
+}
+
+fun customImageLoader(context: Context): ImageLoader {
+    return ImageLoader.Builder(context)
+        .memoryCache {
+            MemoryCache.Builder(context)
+                .maxSizePercent(0.25) // Use 25% of app's memory for caching
+                .build()
+        }
+        .diskCache {
+            DiskCache.Builder()
+                .directory(context.cacheDir.resolve("image_cache"))
+                .maxSizeBytes(10L * 1024 * 1024) // 10 MB
+                .build()
+        }
+        .logger(DebugLogger())
+        .respectCacheHeaders(false)
+        .build()
 }
